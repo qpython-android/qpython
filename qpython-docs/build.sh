0fef3f0c05f1372daa9cafdf65f8a82b366ebc1d
River <riverfor@gmail.com>
2017-04-11 12:17:21 +0800

    Update images

--- qpython-docs/build.sh
+++ qpython-docs/build.sh
@@ -5,6 +5,6 @@ make html
 #mv static static
 #mv sources sources
 
-cd build/html && find . -name "*.html" -exec sed -i -e 's/_static/static/g' {} \; && rm *-e && mv _static static
+cd build/html && find . -name "*.html" -exec sed -i -e 's/_static/static/g;s/_images/images/g' {} \; && rm *-e && mv _static static && mv _images  images
 
 cd ../.. && mv build/html  ../docs && cp CNAME ../docs
