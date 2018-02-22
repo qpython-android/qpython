c92657c5096d46375fc67b4422ce7dd6c07c279d
River <riverfor@gmail.com>
2018-02-22 19:25:29 +0800

    Update show QPYs

--- qpython/src/main/java/org/qpython/qpy/main/dialog/LogoDialog.java
+++ qpython/src/main/java/org/qpython/qpy/main/dialog/LogoDialog.java
@@ -10,35 +10,36 @@ import android.view.ViewGroup;
 
 import org.qpython.qpy.R;
 //import org.qpython.qpy.databinding.DialogLogoBinding;
+import org.qpython.qpy.databinding.DialogLogoBinding;
 import org.qpython.qpy.main.activity.HomeMainActivity;
 import org.qpython.qpy.main.utils.Bus;
 
 public class LogoDialog extends DialogFragment {
     public static final String TAG = "LogoDialog";
-    //private DialogLogoBinding mBinding;
+    private DialogLogoBinding mBinding;
 
     public static LogoDialog newInstance() {
         return new LogoDialog();
     }
 
-//    @Nullable
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        mBinding = DataBindingUtil.inflate(inflater, R.layout.dialog_logo, container, false);
-//
-//        mBinding.btnQrcode.setOnClickListener(v -> {
-//            Bus.getDefault().post(new HomeMainActivity.StartQrCodeActivityEvent());
-//            dismiss();
-//        });
-//        mBinding.btnScript.setOnClickListener(v -> {
-//            Bus.getDefault().post(new HomeMainActivity.ShowProgramDialogEvent(false));
-//            dismiss();
-//        });
-//        mBinding.btnProject.setOnClickListener(v -> {
-//            Bus.getDefault().post(new HomeMainActivity.ShowProgramDialogEvent(true));
-//            dismiss();
-//        });
-//
-//        return mBinding.getRoot();
-//    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.dialog_logo, container, false);
+
+        mBinding.btnQrcode.setOnClickListener(v -> {
+            Bus.getDefault().post(new HomeMainActivity.StartQrCodeActivityEvent());
+            dismiss();
+        });
+        mBinding.btnScript.setOnClickListener(v -> {
+            Bus.getDefault().post(new HomeMainActivity.ShowProgramDialogEvent(false));
+            dismiss();
+        });
+        mBinding.btnProject.setOnClickListener(v -> {
+            Bus.getDefault().post(new HomeMainActivity.ShowProgramDialogEvent(true));
+            dismiss();
+        });
+
+        return mBinding.getRoot();
+    }
 }
